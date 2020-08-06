7e640173b4dc67981ab170a12472455953422e62
TimWhite <36320909+timwhitez@users.noreply.github.com>
2020-08-06 16:24:33 +0800

    Add files via upload

--- xcraw/tasks.py
+++ xcraw/tasks.py
@@ -16,36 +16,43 @@ warnings.filterwarnings(action='ignore')
 # 需要使用一个装饰器，来管理该任务(函数)
 @app.task
 def save_results(request):
-    print(request)
-    try:
-        f = open('result.csv','a')
-        f.write(request + '\n')
-    finally:
-        f.close()
-    return True
+	print(request)
+	results = request.split(",")
+	if len(results) > 4:
+		try:
+			f = open('alive.csv','a')
+			f.write(request + '\n')
+		finally:
+			f.close()
+		return True
+	elif len(results) >2:
+		 try:
+			f = open('brute.csv','a')
+			f.write(request + '\n')
+		finally:
+			f.close()
+		return True
 
 
-
-
-@app.task(time_limit=1800)
+@app.task(time_limit=1000)
 def send_request(target):
-    cmd = ["/root/xhunter-WebAlive/xalive/w.sh",target]
-    print(cmd)
-    try:
-        output = subprocess.check_output(cmd)
-    except:
-        return None
-    results = ''
-    with open(RS_PATH + '/results/alive_results.csv') as file:
-        next(file)
-        for line in file:
-            results += line
-    with open(RS_PATH + '/results/brute_results.csv') as f:
-        next(f)
-        for line1 in f:
-            results += line1
-    if results:
-        try:
-            return results
-        except:
-            return None
+	cmd = ["/root/xhunter-WebAlive/xalive/w.sh",target]
+	print(cmd)
+	try:
+		output = subprocess.check_output(cmd)
+	except:
+		return None
+	results = ''
+	with open(RS_PATH + '/results/alive_results.csv') as file:
+		next(file)
+		for line in file:
+			results += line
+	with open(RS_PATH + '/results/brute_results.csv') as f:
+		next(f)
+		for line1 in f:
+			results += line1
+	if results:
+		try:
+			return results
+		except:
+			return None
